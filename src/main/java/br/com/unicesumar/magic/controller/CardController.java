bb4fb7be6807059092ca7fdd3ed2bdf2095b65e1
willyantomaz <willyantomaz2015@gmail.com>
2024-08-22 21:53:58 -0300

    tudo ok

--- src/main/java/br/com/unicesumar/magic/controller/CardController.java
+++ src/main/java/br/com/unicesumar/magic/controller/CardController.java
@@ -6,12 +6,18 @@ import br.com.unicesumar.magic.enums.CardType;
 import br.com.unicesumar.magic.repository.CardRepository;
 import br.com.unicesumar.magic.repository.DeckRepository;
 import br.com.unicesumar.magic.service.CardService;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.security.PermitAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/card")
@@ -29,19 +35,20 @@ public class CardController {
     private Deck deck;
 
     @GetMapping("/commander")
-    @PermitAll
-    public ResponseEntity<Card> getCommander(@RequestBody Card name){
+
+    public ResponseEntity getCommander(@RequestBody Card name, @RequestParam int qntdCard) {
 
         Card retorno = cardService.getCommanderCard(name.getName());
 
 
-        if(retorno.getCardType().equals(CardType.COMMANDER)){
-            retorno.setResponse("Carta Adicionada com sucesso");
-            //cardRepository.save(retorno);
+        if (retorno.getCardType().equals(CardType.COMMANDER)) {
             deck = new Deck();
             this.deck.setCommander(retorno);
-            this.deck.setCards(cardService.getCommonCard());
+            this.deck.setCards(cardService.getCommonCard(qntdCard, retorno.getColors()));
+
             deckRepository.save(this.deck);
+            saveCardsToFile(this.deck, "src/main/resources/deck.json");
+            retorno.setResponse("Carta Adicionada com sucesso");
 
             return ResponseEntity.ok(retorno);
         }
@@ -49,5 +56,16 @@ public class CardController {
         return ResponseEntity.badRequest().body(retorno);
 
 
+    }
+
+    public void saveCardsToFile(Deck deck, String filePath) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File(filePath), deck);
+            System.out.println("Cartas salvas com sucesso em " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao salvar cartas em arquivo JSON");
+        }
     }
 }
--- src/main/java/br/com/unicesumar/magic/service/CardService.java
+++ src/main/java/br/com/unicesumar/magic/service/CardService.java
@@ -1,11 +1,11 @@
 package br.com.unicesumar.magic.service;
 
-import br.com.unicesumar.magic.config.RestTemplateConfig;
 import br.com.unicesumar.magic.entity.Card;
+import br.com.unicesumar.magic.entity.ScryfallResponse;
 import br.com.unicesumar.magic.enums.CardType;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +17,12 @@ public class CardService {
 
     private final String API_URL = "https://api.scryfall.com/cards";
     private final String API_URL_NAMED = "/named?fuzzy=";
-
+    private final String API_URL_CARDS = "/search?q=color:%s&unique=cards&page=";
 
 
     private List<Card> cards = new ArrayList<>();
 
-    public Card getCommanderCard(String name)  {
+    public Card getCommanderCard(String name) {
 
         String url = API_URL + API_URL_NAMED + name;
         Card commander = restTemplate().getForObject(url, Card.class);
@@ -36,23 +36,35 @@ public class CardService {
         return commander;
     }
 
-    public List<Card> getCommonCard() {
-        String urlCardRandom = "/random";
-        String url = API_URL + urlCardRandom;
-        //try {
-            Card common = restTemplate().getForObject(url, Card.class);
-            common.setCardType(CardType.COMMON);
+    public List<Card> getCommonCard(int qntdCard, List<String> colors) {
 
-            cards.add(common);
-            return cards;
-//        }catch (Exception e){
-//
-//            System.out.println(e+"vazio");
-//        }
+        List<Card> commonCards = new ArrayList<>();
+        int page = 1;
+        try {
 
 
+            while (commonCards.size() < qntdCard) {
+                String url = String.format(API_URL + API_URL_CARDS, String.join(",", colors)) + page;
+                ScryfallResponse response = restTemplate().getForObject(url, ScryfallResponse.class);
+                if (response != null && response.getData() != null) {
+                    for (Card card : response.getData()) {
+                        if (commonCards.size() >= qntdCard) {
+                            break;
+                        }
+                        commonCards.add(card);
+                    }
+                }
+                page++;
+            }
+
+            return commonCards;
+
+
+        } catch (HttpClientErrorException.UnprocessableEntity e) {
+          throw  new RuntimeException("cards insuficientes dessa cor para completar o deck");
+        }
     }
- }
+}
 
 
 
