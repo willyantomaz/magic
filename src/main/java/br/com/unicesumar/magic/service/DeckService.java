3be51bd2588a941afb1dfd553b9e63b2c13e0232
unknown <andreluisjacobroman@gmail.com>
2024-09-28 17:46:37 -0300

    Aplicando cache nas requisições de decks

--- src/main/java/br/com/unicesumar/magic/service/DeckService.java
+++ src/main/java/br/com/unicesumar/magic/service/DeckService.java
@@ -3,6 +3,7 @@ package br.com.unicesumar.magic.service;
 import br.com.unicesumar.magic.entity.Deck;
 import br.com.unicesumar.magic.repository.DeckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,10 +20,12 @@ public class DeckService {
         return this.deckRepository.save(deck);
     }
 
+    @Cacheable("User decks")
     public List<Deck> findDecksByUser (String token) {
         return this.deckRepository.findDeckByUser(this.jwtTokenService.getUserByToken(token.replace("Bearer ", "")));
     }
 
+    @Cacheable("All Decks")
     public List<Deck> findAllDecks () {
         return this.deckRepository.findAll();
     }
